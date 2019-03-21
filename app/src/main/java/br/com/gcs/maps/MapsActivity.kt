58766ff90b19cce861c3a7630c6c246f2a6353a0
gustavocorreia <gustavocorreia.gc@gmail.com>
2019-03-20 22:57:04 -0300

    Ultimas Alterações

--- app/src/main/java/br/com/gcs/maps/MapsActivity.kt
+++ app/src/main/java/br/com/gcs/maps/MapsActivity.kt
@@ -1,26 +1,87 @@
 package br.com.gcs.maps
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import br.com.gcs.maps.utils.PermissionUtils
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var locationManager: LocationManager
+    private  lateinit var locationListener: LocationListener
+
+
+    fun initLocationListener(){
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(location: Location?) {
+                val minhaLocalizacao = LatLng(location?.latitude!!, location?.longitude!!)
+                mMap.addMarker((MarkerOptions().position(minhaLocalizacao)))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(minhaLocalizacao, 15f))
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        PermissionUtils.validarPermissoes(listOf(Manifest.permission.ACCESS_FINE_LOCATION), this, 1)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        for(permissao in grantResults){
+            if(permissao == PackageManager.PERMISSION_DENIED){
+                Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
+            } else{
+                requestLocationUpdates()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun requestLocationUpdates(){
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener
+        )
     }
 
     /**
@@ -35,9 +96,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        initLocationListener()
+        requestLocationUpdates()
+
+        mMap.setOnMapClickListener {
+            mMap.addMarker((MarkerOptions().position(it)))
+        }
+
+        mMap.setOnMapLongClickListener {
+            mMap.addMarker((MarkerOptions().position(it)))
+        }
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val local = LatLng(-23.385163,-46.3292144)
+        mMap.addMarker(MarkerOptions().position(local).title("Hey, Dude"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(local, 15f))
+
+        adicionarFormaCircular(local)
+    }
+
+    private fun  adicionarFormaCircular(latLng: LatLng){
+        val circulo = CircleOptions()
+        circulo.center(latLng)
+        circulo.radius(200.0)
+        circulo.fillColor(Color.argb(128, 0, 51, 102))
+        circulo.strokeColor(Color.argb(128, 0, 51, 102))
+        mMap.addCircle(circulo)
     }
 }
---
+++ app/src/main/java/br/com/gcs/maps/utils/PermissionUtils.kt
@@ -0,0 +1,32 @@
+package br.com.gcs.maps.utils
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import br.com.gcs.maps.MapsActivity
+import java.util.ArrayList
+
+object PermissionUtils {
+    fun validarPermissoes(permissoes: List<String>, activity: Activity, requestCode: Int) : Boolean{
+        val listaPermissoes = ArrayList<String>()
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M){
+            for (permissao in permissoes){
+                val temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED
+                if(!temPermissao)
+                    listaPermissoes.add(permissao)
+            }
+
+            if(listaPermissoes.isEmpty())
+                return true
+            else{
+
+                ActivityCompat.requestPermissions(activity, listaPermissoes.toTypedArray(), requestCode)
+            }
+        }
+
+        return true
+    }
+}
